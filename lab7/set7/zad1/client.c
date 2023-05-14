4087d674f156a9ed708f17fbe1e74fd095d54327
Aleksandra Sobiesiak <aleksandrasobiesiak.2@gmail.com>
2023-05-14 17:08:05 +0200

    all c files created and header.h written + Makefile

---
+++ lab7/set7/zad1/client.c
@@ -0,0 +1,6 @@
+#include "header.h"
+#include <stdio.h>
+
+int main() {
+
+}
--- lab7/set7/zad1/header.h
+++ lab7/set7/zad1/header.h
@@ -1,47 +1,37 @@
-// Wykorzystując semafory i pamięć wspólną z IPC Systemu V oraz standardu POSIX napisz program symulujący działanie salonu fryzjerskiego: 
+#include <stdbool.h>
 
-// W salonie dostępnych jest: 
-
-// M fryzjerów, N foteli (przy czym M >=N) , oraz poczekalnia z P miejscami. 
-
-// Każdy fryzjer albo oczekuje na klienta (śpi), albo obsługuje klienta o ile taki się zgłosił bądź oczekiwał w poczekalni, 
-// fryzjer nie był zajęty, a w salonie dostępny był wolny fotel. 
-
-// Kiedy do salony przychodzi klient, budzi fryzjera (jeżeli jest jakiś śpiący), po czym fryzjer znajduje wolny fotel i obsługuje klienta. 
-// Jeśli nie ma wolnego fotela, klient zajmuje jedno z wolnych miejsc w poczekalni. Jeśli nie ma miejsca w poczekalni, klient odchodzi.
-
-// Należy zsynchronizować prace fryzjerów oraz obsługę klientów w taki sposób, aby jeden fryzjer w danej chwili obsługiwał co najwyżej jednego klienta, 
-// jeden klient był obsługiwany przez co najwyżej jednego fryzjera, żaden z klientów ani żaden z fryzjerów nie został zagłodzony.
-
-// Zakładamy, że fryzjerzy obsługują kolejnych klientów w kolejności zgłaszania się (i/lub w kolejności zajmowanego miejsca w poczekalni) 
-// w miarę dostępnych foteli oraz to ze każdy z fryzjerów potrafi wykonać każdą z oferowanych przez salon fryzur.  
-
-// Przyjmij ze czas wykonania kazdej z F oferowanych przez salon fryzur wynosi kilka sekund, i ze czasy te roznia sie dla kazdej z fryzur. 
-
-// Zaimplementuj powyzszy symulator, wykorzystując mechanizmy synchronizacji procesów oraz pamięć współdzieloną ze standardu:
-
-// IPC - System V (60%)
-
-#ifndef __HEADER_H__
-#define __HEADER_H__
-
+// hairsalon simulation numbers
 #define HAIRDRESSER_TOTAL 3 // hairdressers
 #define CHAIR_TOTAL 2 // chairs
-#define WAITROOM_TOTAL 5 // waiting room
+#define QUEUE_TOTAL 5 // waiting room  - queue
 #define HAIRCUT_TOTAL 4 // number of hairstyles
 #define BUFFER_SIZE 1024
 
-typedef struct
-{
-    int taken; //is occupied now
-    int haircut; //chosen haircut
-} Chair;
+// queue
+char queue_pop(char*);
+void queue_push(char*, char);
+bool queue_full(char*);
+bool queue_empty(char*);
 
-typedef struct {
-    int free_seats;
-    int next_seat;
-    int next_client;
-} WaitRoom;
+// library
+typedef int Semaphore;
+#define SQUEUE_NAME "0"
+#define SCHAIRS_NAME "1"
+#define SBARBERS_NAME "2"
+#define SBUFFER_NAME "3"
+#define NAME getenv("HOME")
+#define BARBER_EXECUTABLE "./barber"
+#define CLIENT_EXECUTABLE "./client"
 
+// memory
+char* shared_memory_init(const char*, int);
+bool shared_memory_detach(char*);
+bool shared_memory_clean(const char*);
 
-#endif
+// semaphore
+Semaphore sem_create(const char*, int);
+Semaphore sem_open(const char*);
+void sem_close(Semaphore);
+void sem_unlink(const char*);
+void sem_wait_for(Semaphore);
+void sem_release(Semaphore);
