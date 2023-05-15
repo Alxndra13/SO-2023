022c7d8138d44a234a60c3ffdd89909565f4163f
Aleksandra Sobiesiak <aleksandrasobiesiak.2@gmail.com>
2023-05-15 20:19:12 +0200

    lab7 zad1 working perfectly now

--- lab8/set8/zad1/life.c
+++ lab8/set8/zad1/life.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <pthread.h>
+#include <signal.h>
 #include "grid.h"
 
 const int num_threads = GRID_WIDTH * GRID_HEIGHT;
@@ -20,11 +21,28 @@ typedef struct ThreadData
     char *dst;
 } ThreadData;
 
-void *thread_worker(void *arg)
+void handle_update(int signum, siginfo_t *info, void *abc)
 {
-    ThreadData *data = (ThreadData *)arg;
+    ThreadData *data = (ThreadData *)info->si_value.sival_ptr;
     data->dst[data->row * GRID_WIDTH + data->col] = is_alive(data->row, data->col, data->src);
-    return NULL;
+    free(data);
+}
+
+void set_signal_action()
+{
+    struct sigaction action;
+    action.sa_sigaction = handle_update;
+    sigemptyset(&action.sa_mask);
+    action.sa_flags = SA_SIGINFO;
+    sigaction(SIGUSR1, &action, NULL);
+}
+
+void *thread_worker()
+{
+    while (true)
+    {
+        pause();
+    }
 }
 
 void create_threads(char *foreground, char *background)
@@ -34,12 +52,25 @@ void create_threads(char *foreground, char *background)
     {
         for (int j = 0; j < GRID_WIDTH; ++j)
         {
+            pthread_create(&threads[i][j], NULL, thread_worker, NULL);
+        }
+    }
+}
+
+void update_threads(char *foreground, char *background)
+{
+    for (int i = 0; i < GRID_HEIGHT; ++i)
+    {
+        for (int j = 0; j < GRID_WIDTH; ++j)
+        {
             ThreadData *thread_data = malloc(sizeof(ThreadData));
             thread_data->src = foreground;
             thread_data->dst = background;
             thread_data->row = i;
             thread_data->col = j;
-            pthread_create(&threads[i][j], NULL, thread_worker, (void *)thread_data);
+            union sigval val;
+            val.sival_ptr = (void *)thread_data;
+            pthread_sigqueue(threads[i][j], SIGUSR1, val);
         }
     }
 }
@@ -60,20 +91,23 @@ int main()
     srand(time(NULL));
     setlocale(LC_CTYPE, "");
     initscr(); // Start curses mode
+    set_signal_action();
 
     char *foreground = create_grid();
     char *background = create_grid();
     char *tmp;
 
     init_grid(foreground);
+    create_threads(foreground, background);
 
     while (true)
     {
         draw_grid(foreground);
         usleep(500 * 1000);
 
-        create_threads(foreground, background);
-        wait_for_threads();
+        // create_threads(foreground, background);
+        // wait_for_threads();
+        update_threads(foreground, background);
 
         tmp = foreground;
         foreground = background;
